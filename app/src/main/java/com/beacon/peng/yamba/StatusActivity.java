d52a4a44ced7f51b73d0972b8e62e6b22eefaca2
hpsoar <hpsoar@gmail.com>
2014-08-31 01:31:34 +0800

    fragment

--- app/src/main/java/com/beacon/peng/yamba/StatusActivity.java
+++ app/src/main/java/com/beacon/peng/yamba/StatusActivity.java
@@ -1,29 +1,29 @@
 package com.beacon.peng.yamba;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.marakana.android.yamba.clientlib.YambaClient;
-import com.marakana.android.yamba.clientlib.YambaClientException;
 
 
 public class StatusActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_activity_status);
+
+        // 1.
+        //setContentView(R.layout.new_activity_status);
+
+        // 2. alternative
+
+        if (savedInstanceState == null) { //
+            // Create a fragment
+            StatusFragment fragment = new StatusFragment(); //
+
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment,
+                            fragment.getClass().getSimpleName())
+                    .commit(); //
+
+        }
     }
 }
