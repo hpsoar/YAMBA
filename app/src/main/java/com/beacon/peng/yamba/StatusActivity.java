b088d423bfaf686745f3d66417d39ffe38517ac4
hpsoar <hpsoar@gmail.com>
2014-08-31 01:21:32 +0800

    added fragment

--- app/src/main/java/com/beacon/peng/yamba/StatusActivity.java
+++ app/src/main/java/com/beacon/peng/yamba/StatusActivity.java
@@ -20,94 +20,10 @@ import com.marakana.android.yamba.clientlib.YambaClient;
 import com.marakana.android.yamba.clientlib.YambaClientException;
 
 
-public class StatusActivity extends Activity implements OnClickListener {
-    private static final String TAG = "StatusActivity";
-
-    private Button buttonTweet;
-    private EditText editStatus;
-    private TextView textCount;
-    private int defaultTextColor;
+public class StatusActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-        buttonTweet = (Button) findViewById(R.id.buttonTweet);
-        editStatus = (EditText) findViewById(R.id.editStatus);
-
-        textCount = (TextView) findViewById(R.id.textCount);
-        defaultTextColor = textCount.getTextColors().getDefaultColor();
-
-        buttonTweet.setOnClickListener(this);
-
-        editStatus.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                int count = 140 - editStatus.length(); //
-                textCount.setText("you can input " + Integer.toString(count) + " more characters");
-                textCount.setTextColor(Color.GREEN); //
-                if (count < 10)
-                    textCount.setTextColor(Color.RED);
-                else
-                    textCount.setTextColor(defaultTextColor);
-            }
-        });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.status, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View view) {
-        String status = editStatus.getText().toString();
-        Log.d(TAG, "onClicked with status: " + status);
-
-
-    }
-
-    private final class PostTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... strings) {
-            YambaClient yambaClient = new YambaClient("username", "password");
-            try {
-                yambaClient.postStatus(strings[0]);
-                return "Successfully posted";
-            } catch (YambaClientException e) {
-                return "Failed to post to yamba service";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Toast.makeText(StatusActivity.this, s, Toast.LENGTH_LONG);
-        }
+        setContentView(R.layout.new_activity_status);
     }
 }
